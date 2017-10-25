5ff76ccfd3d52714290c6ee85ca86b04c46ece83
gloria <gloriasoh553@hotmail.com>
2017-10-25 23:23:01 +0800

    set up devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+end
